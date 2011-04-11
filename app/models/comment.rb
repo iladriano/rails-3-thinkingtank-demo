2ee4eac268d244651a2f03c7766d90e3491da566
reddit-it env <dev@flaptor.com>
2011-04-11 04:34:52 +0000

    mods

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,11 @@
 class Comment < ActiveRecord::Base
   belongs_to :post
+  define_index do
+    indexes commenter
+    indexes postname
+  end
+
+  def postname
+    self.post.title
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments
-  define_index do
-    indexes title
-  end
+#  define_index do
+#    indexes title
+#  end
 end
